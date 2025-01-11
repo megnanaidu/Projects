c481a7ad71df2b42c7530e2fd9de66786b5f1eff
megnanaidu <143658193+megnanaidu@users.noreply.github.com>
2025-01-11 12:08:43 -0500

    Update anagram.cpp

--- anagrams/anagram.cpp
+++ anagrams/anagram.cpp
@@ -18,9 +18,7 @@ AnagramDict::AnagramDict(std::string filename) {
     std::string path = "dictionaries/" + filename_;
     std::ifstream file;
     file.open(path.c_str());
-    /**
-        Your code goes below here
-    **/
+   
 	if (file.is_open())
 	{ //check if file is open
         	std::string cur_word; //create a temp var that holds the list of words
@@ -44,9 +42,7 @@ Output:
 std::string - the sorted version of the input
 ***/
 std::string AnagramDict::sortWord(std::string word) {
-    /**
-        Your code goes below here
-    **/
+  
 	std::sort(word.begin(), word.end()); // Sort letters in word
    	return word;
 }
@@ -64,9 +60,7 @@ Output:
 N/A
 ***/
 void AnagramDict::addWord(std::string word) {
-    /**
-        Your code goes below here
-    **/
+   
 
 	std::string sorted = sortWord(word); //sort the word to check for anagrams
     	Node<std::string, std::list<std::string> > * anagramNode = tree.find_node(sorted); // find the current node in the tree
@@ -109,9 +103,6 @@ void AnagramDict::saveAnagrams(std::string order) {
         //create new file
         file.open(path.c_str(),std::ios::out);
     }
-    /**
-        Your code goes below here
-    **/
 	
 	std::list<Node<std::string,std::list<std::string> > > nodeList;
    	if(order == "pre") 
